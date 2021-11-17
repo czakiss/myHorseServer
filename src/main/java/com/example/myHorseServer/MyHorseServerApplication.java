6783d3791c86970b7fd8ad6e3dc1827e2795d2a0
Kinga <kinga2233@onet.pl>
2021-11-17 23:49:33 +0100

    base

---
+++ src/main/java/com/example/myHorseServer/MyHorseServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.myHorseServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHorseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHorseServerApplication.class, args);
+	}
+
+}
