777e3e2176946fc16187346e1338e9e7ff2ff3b1
Mereti <52887165+Mereti@users.noreply.github.com>
2021-12-03 14:58:28 +0100

    Revert "Feature/kr register service"

--- src/main/java/com/example/myHorseServer/dto/gamerStud/GamerStudDeleteResponse.java
+++
@@ -1,8 +0,0 @@
-package com.example.myHorseServer.dto.gamerStud;
-
-
-public class GamerStudDeleteResponse {
-    private GamerStudDataDto gamerStudData;
-    private String message;
-    //private GamerStudService.Delete_Status deleteStatus;
-}
